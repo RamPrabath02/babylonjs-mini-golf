621181122f9c3e5804aa6ad348ad391cb472c374
Ram Prabath <90195042+RamPrabath02@users.noreply.github.com>
2024-10-15 12:16:21 +0530

    comment changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef } from "react";
 import * as BABYLON from "babylonjs";
 import * as CANNON from "cannon";
 import "babylonjs-loaders";
-import "./App.css"; // Import your styles here
+import "./App.css";
 
 const App = () => {
   const canvasRef = useRef(null);
@@ -11,13 +11,13 @@ const App = () => {
     const canvas = canvasRef.current;
     const engine = new BABYLON.Engine(canvas, true);
 
-    // Attach CANNON to the global window object so Babylon.js can use it
+    // cannon to windown
     window.CANNON = CANNON;
 
     const MAX_FORCE = 10;
     let powerMeter = 0;
 
-    // Power bar update function
+    // power bar
     const updatePowerBar = (power) => {
       const powerBar = document.getElementById("power-bar");
       const maxPowerBarWidth = 150;
@@ -27,7 +27,7 @@ const App = () => {
       )}px`;
     };
 
-    // Reset power bar
+    // reset power width value
     const resetPowerBar = () => {
       document.getElementById("power-bar").style.width = "0px";
     };
@@ -35,13 +35,12 @@ const App = () => {
     const createScene = () => {
       const scene = new BABYLON.Scene(engine);
 
-      // Enable physics with Cannon.js
+      // physics to the scene
       scene.enablePhysics(
         new BABYLON.Vector3(0, -9.81, 0),
         new BABYLON.CannonJSPlugin()
       );
 
-      // Camera
       const camera = new BABYLON.ArcRotateCamera(
         "ArcRotateCamera",
         Math.PI / 2,
@@ -55,17 +54,15 @@ const App = () => {
       camera.upperRadiusLimit = 30;
       camera.panningSensibility = 0;
 
-      // Light
       const light = new BABYLON.HemisphericLight(
         "light",
         new BABYLON.Vector3(0, 1, 0),
         scene
       );
 
-      // Walls
       const wallMaterial = new BABYLON.StandardMaterial("wallMaterial", scene);
       wallMaterial.diffuseColor = new BABYLON.Color3(0.2, 0.2, 0.2);
-
+      //function to create a wall with a name, width, height, depth and position attributes
       const createWall = (name, width, height, depth, position) => {
         const wall = BABYLON.MeshBuilder.CreateBox(
           name,
@@ -88,7 +85,7 @@ const App = () => {
       createWall("backWall", 10, 2, 1, new BABYLON.Vector3(0, 1, 30));
       createWall("frontWall", 10, 2, 1, new BABYLON.Vector3(0, 1, 0));
 
-      // Golf course ground
+      // to create my simple course
       const box = BABYLON.MeshBuilder.CreateBox(
         "box",
         { height: 1, width: 10, depth: 40 },
@@ -99,7 +96,7 @@ const App = () => {
       box.material = new BABYLON.StandardMaterial("boxMat", scene);
       box.material.diffuseColor = new BABYLON.Color3(0.13, 0.55, 0.13);
 
-      // Hole
+      // hole cylinder mesh
       const holeDiameter = 2.5;
       const hole = BABYLON.MeshBuilder.CreateCylinder(
         "hole",
@@ -108,7 +105,7 @@ const App = () => {
       );
       hole.position.y = 1;
       hole.position.z = 20;
-
+      // csg to cut(hole) from the box mesh
       const boxCSG = BABYLON.CSG.FromMesh(box);
       const holeCSG = BABYLON.CSG.FromMesh(hole);
       const boxWithHole = boxCSG
@@ -124,7 +121,7 @@ const App = () => {
         scene
       );
 
-      // Goal
+      // goal mesh
       const goal = BABYLON.MeshBuilder.CreateCylinder(
         "goal",
         { diameter: holeDiameter, height: 0.2 },
@@ -142,7 +139,7 @@ const App = () => {
         scene
       );
 
-      // Ball
+      // sphere used as ball
       const ball = BABYLON.MeshBuilder.CreateSphere(
         "ball",
         { diameter: 0.5 },
@@ -164,12 +161,12 @@ const App = () => {
           friction: 0,
           restitution: 0.1,
           damping: 0.3,
-          angularDamping: 1.0,
+          angularDamping: 1.0, //to cut down spinning
         },
         scene
       );
 
-      // Arrow for direction indicator
+      //arrow
       const arrow = BABYLON.MeshBuilder.CreateCylinder(
         "arrow",
         { diameterTop: 0, diameterBottom: 0.2, height: 2, tessellation: 96 },
@@ -178,7 +175,7 @@ const App = () => {
       arrow.rotation.x = Math.PI / 2;
       arrow.isVisible = false;
 
-      // Check if the ball reaches the goal
+      // hole function to show finish screen(distance based on ball and goal)
       scene.registerBeforeRender(() => {
         const distanceToTrigger = BABYLON.Vector3.Distance(
           ball.position,
@@ -189,12 +186,13 @@ const App = () => {
         }
 
         const velocity = ball.physicsImpostor.getLinearVelocity();
+        //get speed so i can use for stopping
         const speed = Math.sqrt(
           velocity.x * velocity.x +
             velocity.y * velocity.y +
             velocity.z * velocity.z
         );
-
+        // to stop ball
         if (speed < 0.5) {
           ball.physicsImpostor.setLinearVelocity(BABYLON.Vector3.Zero());
           ball.physicsImpostor.setAngularVelocity(BABYLON.Vector3.Zero());
@@ -213,7 +211,7 @@ const App = () => {
     };
 
     const { scene, ball, camera, arrow } = createScene();
-
+    // continously rendering with ball position as camera target
     engine.runRenderLoop(() => {
       scene.render();
       camera.setTarget(ball.position);
@@ -222,7 +220,7 @@ const App = () => {
     window.addEventListener("resize", () => {
       engine.resize();
     });
-
+    //logic for dragging
     let isDragging = false;
     let dragStartPos = new BABYLON.Vector3();
     let dragEndPos = new BABYLON.Vector3();
@@ -234,48 +232,52 @@ const App = () => {
       if (pickResult.hit && pickResult.pickedMesh === ball) {
         isDragging = true;
         dragStartPos.copyFrom(pickResult.pickedPoint);
-
+        // no pannning after the drag starts pointee down
         camera.lowerBetaLimit = camera.beta;
         camera.upperBetaLimit = camera.beta;
         camera.lowerRadiusLimit = camera.radius;
         camera.upperRadiusLimit = camera.radius;
-
+        //show arrow
         arrow.isVisible = true;
         arrow.position = ball.position;
         arrow.scaling.z = 0.1;
       }
     });
 
+    // function to find drag force in anydirection
     canvas.addEventListener("pointermove", (evt) => {
       if (!isDragging) return;
 
       const pickResult = scene.pick(evt.clientX, evt.clientY);
+
       if (pickResult.hit) {
+        // drag distance
         dragEndPos.copyFrom(pickResult.pickedPoint);
         dragDistance = BABYLON.Vector3.Distance(dragStartPos, dragEndPos);
         const dragVector = dragStartPos.subtract(dragEndPos);
         dragVector.y = 0;
-
+        //direction of the arrow
         arrow.position = ball.position;
         arrow.scaling.z = Math.min(dragDistance * 0.2, 5);
+        // atan2 to find direction of the arrow
         arrow.rotation.y = Math.atan2(dragVector.x, dragVector.z);
-
+        //update the power value for the width
         powerMeter = Math.min(dragDistance * 2, MAX_FORCE);
         updatePowerBar(powerMeter);
       }
     });
-
+    //to applyforce with direction
     canvas.addEventListener("pointerup", () => {
       if (isDragging) {
         isDragging = false;
-
+        //camera reset
         camera.lowerBetaLimit = null;
         camera.upperBetaLimit = null;
         camera.lowerRadiusLimit = 5;
         camera.upperRadiusLimit = 30;
 
         arrow.isVisible = false;
-
+        //arrow direction
         const dragVector = dragStartPos.subtract(dragEndPos);
         dragVector.y = 0;
         const forceMagnitude = powerMeter;
@@ -304,9 +306,10 @@ const App = () => {
 
   return (
     <>
+      {/* <--to render the canvas --> */}
       <canvas ref={canvasRef} id="renderCanvas"></canvas>
       <div id="hud">
-        <div id="score-info">Shots: 0</div>
+        <div id="score-info">Shots: </div>
         <div id="power-bar-container">
           <div id="power-bar"></div>
         </div>
